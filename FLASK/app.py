d1a28275378d5512aa8808074391cf5bb2381bfe
Ahmad <ahmadzakariya.jk@gmail.com>
2025-07-03 20:52:17 +0530

    Minor Changes

--- FLASK/app.py
+++ FLASK/app.py
@@ -161,6 +161,7 @@ def search():
     return render_template("search.html", query=query, results=results)
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=8000)
+   with app.app_context():
+      db.create_all()
+app.run(debug=True, port=8000)
+    
